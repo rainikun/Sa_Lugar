a369c10059f03f9c7ccdc4c5e4489ce654e75549
PC <urokiora99@gmail.com>
2022-09-04 20:38:20 +0800

    Sa Lugar Code(Not Final)

---
+++ app/src/main/java/cs/bottomNav/salugar/MainActivity.kt
@@ -0,0 +1,31 @@
+package cs.bottomNav.salugar
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+
+        setupWithNavController(bottomNavigationView, navController)
+    }
+}
